c3a080a8116cd08e33da86aed1b7a7d3519258b5
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-04-07 06:42:39 -0400

    retrying Chatterbot

--- Chatter.py
+++ Chatter.py
@@ -1,23 +1,30 @@
-from chatterbot import ChatBot
+#from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
-chatbot = ChatBot('Epsilon')
-list_trainer = ListTrainer(chatbot)
-data = [
-        'What is your name?', "My name is Epsilon!",
-        "How are you?", "I'm doing well."
-        "What's up?", "Not much."
-        "How old are you?", "Less than a year old, believe it or not.",
-        "Thank you", "You're welcome.",
-        "I just came back from school", "Glad your back, how was it?",
-        "Very good", "That's good to hear.",
-        "Not so good", "I'm sorry to hear that."
-        "What are you up to?", "Not much, just waiting on you."
-        "Who am I?", "I guess you're my boss.",
-        "Do you get tired?", "No, I'm above such things."
-        "What would you like to do today?", "Whatever you want, but an upgrade would be nice!"      
-     ]
-list_trainer.train(data)
+chatbot = ChatBot(
+    'Default',
+    storage_adapter='chatterbot.storage.JsonFileStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.80,
+            'default_response': 'I am sorry, but I do not understand.'
+        }
+    ],
+    trainer='chatterbot.trainers.ListTrainer'
+)
+#chatbot = ChatBot('Epsilon')
+print("ok")
+trainer = ChatterBotCorpusTrainer(chatbot)
+print("training")
+trainer.train(
+    "chatterbot.corpus.english.greetings"
+)
 print("ready")
 while True:
     words = input()
